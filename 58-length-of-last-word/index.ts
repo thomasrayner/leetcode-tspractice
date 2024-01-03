d7b08adde36f5ad353e3d7677425b1b3dc3e1f1f
Thomas Rayner <thmsrynr@outlook.com>
2024-01-03 12:44:55 -0800

    58 better

--- 58-length-of-last-word/index.ts
+++ 58-length-of-last-word/index.ts
@@ -1,4 +1,3 @@
 function lengthOfLastWord(s: string): number {
-    let r = s.split(" ").map(x => x.trim()).filter(x => x != "");
-    return r[r.length - 1].length;
+    return s.trimEnd().split(" ").pop().length;
 };

2f1e956bffbbe128d091c9e783a4edd98347253f
Thomas Rayner <thmsrynr@outlook.com>
2024-01-03 08:35:34 -0800

    274

---
+++ 274-h-index/index.ts
@@ -0,0 +1,11 @@
+function hIndex(citations: number[]): number {
+    citations.sort((a, b) => b - a);
+
+    for (let i = 0; i < citations.length; i++) {
+        if (i + 1 > citations[i]) {
+            return i;
+        }
+    }
+
+    return citations.length;
+};

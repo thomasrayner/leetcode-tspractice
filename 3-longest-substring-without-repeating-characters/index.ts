409cb34f707be6b718ecab4ae5d8fbb1fffc6b96
Thomas Rayner <thmsrynr@outlook.com>
2024-01-19 09:05:42 -0800

    3

--- 3-longest-substring-without-repeating-characters/index.ts
+++ 3-longest-substring-without-repeating-characters/index.ts
@@ -3,15 +3,15 @@ function lengthOfLongestSubstring(s: string): number {
     let map: Map<string, number> = new Map();
     let l = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        const c = s[i];
+    for (let r = 0; r < s.length; r++) {
+        const c = s[r];
 
         if (map.has(c)) {
             l = Math.max(map.get(c)! + 1, l);
         }
 
-        max = Math.max(max, i - l + 1);
-        map.set(c, i);
+        max = Math.max(max, r - l + 1);
+        map.set(c, r);
     }
 
     return max;
